39a7a97b0c7ecf2ee97dc40b1b85a83c3898a8df
EthanL173 <etl4@pct.edu>
2022-09-20 15:14:49 -0400

    Base for interact
    interact system base

---
+++ CopyRightCommando/Assets/Ethan's Code/Interactor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Interactor : MonoBehaviour
+{
+    public LayerMask interactableLayer;
+    public Interactable interactable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2, interactableLayer))
+        {
+            if(hit.collider.GetComponent<Interactable>() != false)
+            {
+              if(interactable == null || interactable.ID != hit.collider.GetComponent<Interactable>().ID)
+                {
+                    interactable = hit.collider.GetComponent<Interactable>();
+                    
+                }
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    interactable.onInteract.Invoke();
+                }
+            }
+        }
+    }
+}
