e04edc4f6e43d03ac39fdff1a04995c934f3e69b
EthanL173 <etl4@pct.edu>
2022-09-25 12:38:13 -0400

    Last Minute addons
    Made a cooler healthbar and mission text

--- CopyRightCommando/Assets/Ethan's Code/PHealth.cs
+++ CopyRightCommando/Assets/Ethan's Code/PHealth.cs
@@ -9,17 +9,20 @@ public class PHealth : MonoBehaviour
     float currentHealth;
 
     public HealthBar healthBar;
+    public HealthBar healthBar2;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        healthBar2.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+        healthBar2.SetHealth(currentHealth);
     }
 
     
