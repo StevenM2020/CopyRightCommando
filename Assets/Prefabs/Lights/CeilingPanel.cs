1f722b8cbc25e6aff0fecfbb1276b45f70b638ce
Steven <steven.bikerider@gmail.com>
2023-04-12 06:15:07 -0400

    light panels
    I coded a light panel asset
    I added this asset to the office

---
+++ Assets/Prefabs/Lights/CeilingPanel.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering.HighDefinition;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class CeilingPanel : MonoBehaviour
+{
+    public int spawnX, spawnZ;
+    public GameObject tile;
+    public int panelPerLight;
+    public float xMoveBy = 1.1f,
+                 yMoveBy = 1.1f;
+    public Material lightMat;
+    public GameObject light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int counter = 0;
+        for (int i = 0; i < spawnX; i++)
+        {
+            for (int j = 0; j < spawnZ; j++)
+            {
+                GameObject newTile = Instantiate(tile);
+                newTile.transform.parent = transform;
+                newTile.transform.position = new Vector3(gameObject.transform.position.x + i * xMoveBy,
+                                                        gameObject.transform.position.y,
+                                                        gameObject.transform.position.z + j * yMoveBy);
+                if (counter == panelPerLight)
+                {
+                    newTile.transform.parent = gameObject.transform;
+                    newTile.GetComponent<Renderer>().material = lightMat;
+                    GameObject newLight = Instantiate(light);
+                    newLight.transform.position = newTile.transform.position;
+                    counter = 0;
+                }
+                counter++;
+            }
+            
+        }
+    }
+}
